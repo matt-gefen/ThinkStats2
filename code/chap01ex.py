e5061547745be78ea79b463082597f46dc6bfe23
matt-gefen <matt.gefen@gmail.com>
2020-12-19 16:53:09 -0700

    adjuested dict reading script from book

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,12 +13,20 @@ import sys
 import nsfg
 import thinkstats2
 
+def ReadFemResp(
+    dct_file='2002FemResp.dct',
+    dat_file='2002FemResp.dat.gz'):
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip')
+    CleanFemPreg(df)
+    return df
 
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
+
     print('%s: All tests passed.' % script)
 
 
